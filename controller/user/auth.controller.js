259c08758c4fd1e490af5d38d0d95e76e47ae1c1
saurabh <sourabh.impactmindz@gmail.com>
2026-06-10 11:45:07 +0530

    update

--- controller/user/auth.controller.js
+++ controller/user/auth.controller.js
@@ -85,7 +85,7 @@ export const UserLogin = async(req,res) => {
 
         res.cookie("refreshToken",refreshToken,{
             httpOnly : true,
-            secure:false,
+            secure:true,
             sameSite:"lax",
             path:"/",
             maxAge: 7 * 24 * 60 * 60 * 1000  
