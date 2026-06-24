bbc8f01b7fb905dff48fbdc564ae8493b8e6371d
saurabh <sourabh.impactmindz@gmail.com>
2026-06-24 15:16:25 +0530

    changes

--- utils/sendMails.js
+++ utils/sendMails.js
@@ -1,8 +1,11 @@
 import { Resend } from "resend";
+import "dotenv/config";
 
 const resend = new Resend(
   process.env.RESEND_API
 );
+
+console.log(process.env.RESEND_API_KEY);
 
 export const sendOtp = async (email, otp) => {
   try {
